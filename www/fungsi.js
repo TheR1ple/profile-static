ddd6aead8993e7d2492c4b73ee0d2064b90838a3
Rastra Wardana Nanditama <rastra@upi.edu>
2023-06-08 10:54:46 +0700

    update structure and app.yaml

---
+++ www/fungsi.js
@@ -0,0 +1,13 @@
+
+/*skrip javascript untuk fungsi dropdown*/
+    function fungsi() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+    window.onclick = function(e) {
+      if (!e.target.matches('.dropbtn')) {
+      var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+          myDropdown.classList.remove('show');
+        }
+      }
+    }
